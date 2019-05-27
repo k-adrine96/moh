69bd8619f9748c937cb39d1818810b10d83d04b0
Harut Gambaryan <gambaryanharutyun@gmail.com>
2019-05-27 09:36:09 +0400

    added ministry structure

--- src/js/select2.js
+++ src/js/select2.js
@@ -3,5 +3,10 @@
     $('.language--bar').select2({
       minimumResultsForSearch: -1
     });
+    $('.Legal__acts--type').select2({
+      minimumResultsForSearch: -1,
+      placeholder: 'Տեսակ',
+      allowClear: true
+    })
   }
 })(window, jQuery);
