68d8a7a11abffd421905b3aade508552c9c58ae8
adrine.karapetyan <adrine1996@gmail.com>
2019-06-12 17:11:41 +0400

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let LiveReloadPlugin = require('webpack-livereload-plugin');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+.sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.webpackConfig({
   plugins: [new LiveReloadPlugin()]
