7e44568e600182984f431e2af33e4386a52e5084
adrine.karapetyan <adrine1996@gmail.com>
2019-06-14 14:37:00 +0400

    asd

---
+++ app/MinisterHistoryCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class MinisterHistoryCategory extends Model
+{
+    use Translatable;
+
+    protected $fillable = [
+        '',
+    ];
+}
