2f6fd32552d18ea1b2192ca2a9a62e0b153ca9b3
Adrine Karapetyan <adrine1996@gmail.com>
2019-06-26 14:30:41 +0400

    stage files

--- resources/views/layouts/partners.blade.php
+++ resources/views/layouts/partners.blade.php
@@ -1,13 +1,17 @@
 <section class="partners">
     <div class="partners--cont">
         <div class="partners--cont--slider">
+            @if(isset($partnersRow1))
             @foreach($partnersRow1 as $partnerPic)
                 <a href="{{$partnerPic->url}}" class="partners--cont--item" style="background-image: url('{{Storage::url($partnerPic->image)}}')"></a>
             @endforeach
+            @endif
         </div>
         <div class="partners-1--cont--slider">
+            @if(isset($partnersRow2))
             @foreach($partnersRow2 as $partnerPic)
                 <a href="{{$partnerPic->url}}" class="partners--cont--item" style="background-image: url('{{Storage::url($partnerPic->image)}}')"></a>
             @endforeach
+            @endif
     </div>
 </section>
