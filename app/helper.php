5192968d91e3208b91f35ba7a6a6e9784261fe03
adrine.karapetyan <adrine1996@gmail.com>
2019-07-08 15:36:23 +0400

    file exist check

--- app/helper.php
+++ app/helper.php
@@ -44,8 +44,11 @@ if (!function_exists('getFileSize')) {
         for ($i = 0; $size > 1024; $i++) {
             $size /= 1024;
         }
-
-        return  round($size, 2) . ' ' . $units[$i];
+        if(file_exists($file)){
+            return  round($size, 2) . ' ' . $units[$i];
+        }else{
+            return 0;
+        }
 
     }
 
