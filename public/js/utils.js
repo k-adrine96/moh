b84cf364bb325da6cadaa41f2278ad59efeb4631
adrine.karapetyan <adrine1996@gmail.com>
2019-08-05 14:25:56 +0400

    search: Done

--- public/js/utils.js
+++ public/js/utils.js
@@ -17,9 +17,6 @@
     }
   });
 
-
-
-
   window.stopPropagation = function (el, event) {
     $(el).on(event, function (e) {
       e.stopPropagation();
@@ -57,4 +54,3 @@
   }
 
 })(window, jQuery);
-
