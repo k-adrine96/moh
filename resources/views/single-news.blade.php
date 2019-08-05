c6bedacf6462a20dee9d7c6e25a56eea1ad1bad0
adrine.karapetyan <adrine1996@gmail.com>
2019-08-05 16:22:16 +0400

    share

--- resources/views/single-news.blade.php
+++ resources/views/single-news.blade.php
@@ -28,7 +28,6 @@
                     {!! getDescription($newsIndividual->getTranslatedAttribute('description')) !!}
                 </div>
             </div>
-            <a href="https://www.facebook.com/sharer/sharer.php?u=http://moh.mgplab.com&display=popup"> share this </a>
         </section>
         <section class="main__cont__size print-container">
             <a class="print-page" href="javascript:;"></a>
