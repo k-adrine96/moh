16f02adecf2f8243832f05012142dae9cf061cc0
adrine.karapetyan <adrine1996@gmail.com>
2019-08-08 12:58:06 +0400

    fb share check

--- resources/views/sub-structures-drug-medical-expertise.blade.php
+++ resources/views/sub-structures-drug-medical-expertise.blade.php
@@ -1,4 +1,9 @@
-@include('partials.fb-share-meta', ['description' => getDescription($pagesTexts->getTranslatedAttribute('page_text1')) ])
+@section('meta')
+    <meta property="og:url"           content="{{url()->current()}}" />
+    <meta property="og:title"         content="{{$coverPhoto->page_name}}" />
+    <meta property="og:description"   content="{{ strip_tags(getDescription($pagesTexts->getTranslatedAttribute('page_text1'))) }}" />
+    <meta property="og:image"         content="{{ $_SERVER['APP_URL']."/".Storage::url($coverPhoto->page_cover_photo) }}" />
+@endsection
 @extends('layouts.app')
 
 @section('content')
