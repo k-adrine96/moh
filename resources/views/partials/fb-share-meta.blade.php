3e4d698d0f36436342da16d211d28513887cc0ad
adrine.karapetyan <adrine1996@gmail.com>
2019-08-08 13:22:05 +0400

    fb share check

--- resources/views/partials/fb-share-meta.blade.php
+++ resources/views/partials/fb-share-meta.blade.php
@@ -2,5 +2,5 @@
     <meta property="og:url"           content="{{url()->current()}}" />
     <meta property="og:title"         content="{{$coverPhoto->page_name}}" />
     <meta property="og:description"   content="{{ strip_tags($description) }}" />
-    <meta property="og:image"         content="{{ $_SERVER['APP_URL']."/".Storage::url($coverPhoto->page_cover_photo) }}" />
+    <meta property="og:image"         content="{{ $image }}" />
 @endsection
