c6ac55919bff3e5ea6049a63eefc3c168afdfd43
adrine.karapetyan <adrine1996@gmail.com>
2019-09-04 15:42:53 +0400

    success popup header

--- resources/views/partials/popup.blade.php
+++ resources/views/partials/popup.blade.php
@@ -2,13 +2,12 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+{{--                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>--}}
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-
                 <p>{{ $message }}</p>
             </div>
             <div class="modal-footer">
